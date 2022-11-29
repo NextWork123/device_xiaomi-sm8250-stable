394da69a5714061c585a0596678a8ba1c66f2567
NextWork123 <nextdesigns@libero.it>
2022-11-29 21:13:26 +0100

    Revert "sm8250-common: Import GPS lowi.conf to DT & disable debugging"
    This reverts commit ef103cf552d253fcfa62c22c63d5d2f8b609a961.

--- kona.mk
+++ kona.mk
@@ -367,10 +367,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp.permissions-gms.xml
 
-# GPS Configs
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
