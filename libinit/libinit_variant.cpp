a61c1173290804ad2bb84d2c92c2665488dd4819
althafvly <althafvly@gmail.com>
2022-12-24 07:51:56 +0000

    sm8250-common: libinit: Set default Bluetooth name

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -35,6 +35,8 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("mod_device", variant.mod_device, true);
     set_ro_build_prop("model", variant.model, true);
 
+    property_override("bluetooth.device.default_name", variant.marketname);
+
     if (access("/system/bin/recovery", F_OK) != 0) {
         set_ro_build_prop("fingerprint", variant.build_fingerprint);
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
