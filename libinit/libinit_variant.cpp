b9da59b3f26f56a5d15aa540501c44b0d8505761
johnmart19 <johnivan19999@gmail.com>
2023-01-23 05:57:32 +0200

    sm8250-common: Drop Build Fingerprint overwrites
    - First part is VoidUI-Devices/device_xiaomi_alioth@42cfacb05683d88b3d8c1e8d9ac9facf0c75d6e8

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -37,11 +37,6 @@ void set_variant_props(const variant_info_t variant) {
 
     property_override("bluetooth.device.default_name", variant.marketname);
 
-    if (access("/system/bin/recovery", F_OK) != 0) {
-        set_ro_build_prop("fingerprint", variant.build_fingerprint);
-        property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
-    }
-
     if (variant.nfc)
         property_override(SKU_PROP, "nfc");
 }
